e32d3a1a13a479fb39ab033ac4feca77c6ef57ba
MaximumFire <68478521+MaximumFire@users.noreply.github.com>
2022-04-13 00:20:20 +0100

    more multiplayer shit

--- Python/Chess Stuff/Chess/Chess/chessapi.py
+++ Python/Chess Stuff/Chess/Chess/chessapi.py
@@ -7,8 +7,6 @@ api = Api(app)
 
 gs = ChessEngine.GameState()
 valid_moves = None
-move_made = False
-animate = False
 
 
 class Board(Resource):
@@ -30,10 +28,31 @@ class Move(Resource):
                     return {"move_made": True}
         return {"move_made": False}
 
+class Undo(Resource):
+    def get(self):
+        global gs
+        gs.undoMove()
+        return {"done": True}
+
+
+class Reset(Resource):
+    def get(self):
+        global gs, valid_moves
+        gs = ChessEngine.GameState()
+        valid_moves = gs.getValidMoves()
+
+
+class CheckmateOrStalemate(Resource):
+    def get(self):
+        global gs
+        return {"stalemate": gs.stalemate, "checkmate": gs.checkmate}
+
 
 api.add_resource(Board, "/board")
 api.add_resource(Move, "/move/<int:player_is_white>/<int:startrow>/<int:startcol>/<int:endrow>/<int:endcol>")
-api.add_resource(Vars, "/vars/<string:option>")
+#api.add_resource(Vars, "/vars/<string:option>")
+api.add_resource(Undo, "/undo")
+api.add_resource(CheckmateOrStalemate, "/checkorstale")
 
 if __name__ == "__main__":
     app.run(debug=True)
