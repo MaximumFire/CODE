1057e3a8627cce7a16c113945f8e7c3eded2bd09
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2022-08-19 21:43:35 +0100

    testing html stuff

--- Python/test.py
+++ Python/test.py
@@ -1,3 +1,18 @@
-import numpy
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+import sys
 
-print(numpy.cbrt(-27))
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+    def do_OPTIONS(self):
+        self.send_response(200, "ok")
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.end_headers()
+
+if __name__ == '__main__':
+    #test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
+    test(CORSRequestHandler, HTTPServer, port=8080)
