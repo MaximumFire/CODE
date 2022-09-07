8fbda7dd2cde8ef94b14efc8b52ab2fdee680416
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2022-09-07 07:46:05 +0100

    random testing shit

---
+++ AutoHotKey Scripts/test.ahk
@@ -0,0 +1,7 @@
+#SingleInstance, Force
+SendMode Input
+SetWorkingDir, %A_ScriptDir%
+
+^!Ins::Send "nigger"
+
+return
--- Python/test.py
+++ Python/test.py
@@ -1,18 +1,4 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler, test
-import sys
+with open("canoe.txt", "w+") as f:
+  f.write("//Start")
+  f.write("\n\n")
 
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        SimpleHTTPRequestHandler.end_headers(self)
-
-    def do_OPTIONS(self):
-        self.send_response(200, "ok")
-        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
-        self.send_header("Access-Control-Allow-Headers", "Content-Type")
-        self.end_headers()
-
-if __name__ == '__main__':
-    #test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
-    test(CORSRequestHandler, HTTPServer, port=8080)
