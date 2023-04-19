5718b0d25b5e926845c9f4410d3b086c248c84ef
Connor McHarg <connor@mcharg.com>
2023-04-19 22:13:54 +0100

    testing wth gitkraken

--- Python/test.py
+++ Python/test.py
@@ -1,14 +1,1 @@
-x = int(input())
-y = int(input())
-
-out = ""
-
-z = (x/100) + y
-w = x + (y/100)
-
-if z < 20:
-    out += f"£{z}\n"
-if w < 20:
-    out += f"£{w}"
-
-print(out)
+print("hello world")
