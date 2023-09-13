5b9240cda5f1b6a6410bba6cf0fe396cd69da899
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2023-09-13 22:21:03 +0100

    just some coding

---
+++ C#/LaptopTest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LaptopTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
