d0229c00a2e32a08c1b7bdc28d6cf3fc4d28b486
Connor McHarg <68478521+MaximumFire@users.noreply.github.com>
2023-12-21 10:55:43 +0000

    advent of code testing

---
+++ Rust/Advent Of Code/Day 5/day5/target/debug/day5.d
@@ -0,0 +1,1 @@
+C:\Users\conno\OneDrive\Documents\GitHub\CODE\Rust\Advent\ Of\ Code\Day\ 5\day5\target\debug\day5.exe: C:\Users\conno\OneDrive\Documents\GitHub\CODE\Rust\Advent\ Of\ Code\Day\ 5\day5\input.txt C:\Users\conno\OneDrive\Documents\GitHub\CODE\Rust\Advent\ Of\ Code\Day\ 5\day5\src\main.rs
---
+++ Rust/Advent Of Code/Day 5/day5/target/debug/deps/day5.d
@@ -0,0 +1,6 @@
+C:\Users\conno\OneDrive\Documents\GitHub\CODE\Rust\Advent Of Code\Day 5\day5\target\debug\deps\day5.exe: src\main.rs src\../input.txt
+
+C:\Users\conno\OneDrive\Documents\GitHub\CODE\Rust\Advent Of Code\Day 5\day5\target\debug\deps\day5.d: src\main.rs src\../input.txt
+
+src\main.rs:
+src\../input.txt:
